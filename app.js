2d4951bae8b5ac6da8f4f4af1085bca592989326
samuel <samsubas01@gmail.com>
2020-10-30 10:08:03 +0200

    Fixed hamburger animations

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const toggle = document.querySelector('.toggle');
 let tl = gsap.timeline(); 
+let clicked = false;
 
 document.addEventListener('click', (e) => {
-  if(e.target.className === 'toggle-1') {
-    tl.to('.toggle-1' , {rotate: '45deg', duration: 1});
+  if((e.target.className === 'toggle-1' || 'toggle-2' || 'toggle-3') && clicked === false) {
+    tl.to('.toggle-1' , {rotate: '40', transformOrigin: '20% 0%', duration: 0.5})
+      .to('.toggle-2', {opacity: 0 , duration: 0.3}, "-=1")
+      .to('.toggle-3', {rotate: '-40', transformOrigin: '20% 0%', duration: 0.5}, 0.5);
+      clicked = true;
+  } else {
+    // tl.to('.toggle-1' , {rotate: '0', duration: 0.5}, 0.3)
+    //   .to('.toggle-2', {opacity: 1 , duration: 0.3}, 0.3)
+    //   .to('.toggle-3', {rotate: '0', duration: 0.5}, 0.3);
+    clicked = false;
   }
 });
