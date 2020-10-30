af5d9cdd7cc20b34d87841037765e88819351abd
SamuelSub <samsubas01@gmail.com>
2020-10-30 14:57:42 +0200

    Added animation to the mobile nav

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 const toggle = document.querySelector('.toggle');
 let tl = gsap.timeline(); 
-let clicked = false;
 
 document.addEventListener('click', (e) => {
-  if((e.target.className === 'toggle-1' || 'toggle-2' || 'toggle-3') && clicked === false) {
-    tl.to('.toggle-1' , {rotate: '40', transformOrigin: '20% 0%', duration: 0.5})
-      .to('.toggle-2', {opacity: 0 , duration: 0.3}, "-=1")
-      .to('.toggle-3', {rotate: '-40', transformOrigin: '20% 0%', duration: 0.5}, 0.5);
-      clicked = true;
+  if((e.target.className === 'toggle-1') || (e.target.className === 'toggle-2') || (e.target.className === 'toggle-3') && tl.reversed()) {
+    tl.play();
+    const hero = document.querySelector('.hero');
+    const sideMenu = document.createElement('div');
+    sideMenu.className = 'side-menu';
+    hero.appendChild(sideMenu);
+    const ul = document.createElement('ul');
+    ul.className = 'side-ul';
+    const li = document.createElement('li');
+    li.innerHTML =`  
+      <h2>Products</h2>
+      <h2>About</h2>
+      <h2>Contact</h2>`;
+    ul.appendChild(li);
+    sideMenu.appendChild(ul);
+    tl.to('.toggle-1' , {rotate: '40', transformOrigin: '20% 40%', duration: 0.5}, 'dissapear')
+      .to('.toggle-2', {opacity: 0 , duration: 0.3}, 'dissapear')
+      .to('.toggle-3', {rotate: '-40', transformOrigin: '20% 40%', duration: 0.5}, 'dissapear')
+      .from('.side-menu', {x: '-100%', opacity: 1, duration: 0.5}, 'dissapear')
+      .fromTo('h2', {x: '-100%'}, {x: 0, duration: 0.3, stagger: 0.1}, 'dissapear');
   } else {
-    // tl.to('.toggle-1' , {rotate: '0', duration: 0.5}, 0.3)
-    //   .to('.toggle-2', {opacity: 1 , duration: 0.3}, 0.3)
-    //   .to('.toggle-3', {rotate: '0', duration: 0.5}, 0.3);
-    clicked = false;
+    tl.reverse();
   }
 });
