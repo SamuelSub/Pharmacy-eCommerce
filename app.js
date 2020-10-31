3569097674f6e8100ed39fc36687f4b2168eff3b
samuel <samsubas01@gmail.com>
2020-10-31 21:47:01 +0200

    Side menu takes full screen

--- app.js
+++ app.js
@@ -1,28 +1,20 @@
+// GSAP Animations
 const toggle = document.querySelector('.menu-icon');
 const hero = document.querySelector('.hero');
+const sideMenu = document.querySelector('.side-menu');
+const button = document.querySelector('.button');
 let firstToggle = false;
 let tl = gsap.timeline(); 
 
 toggle.addEventListener('click', (e) => {
 
   if(firstToggle === false) {
-    const sideMenu = document.createElement('div');
-    sideMenu.className = 'side-menu';
-    hero.appendChild(sideMenu);
-    const ul = document.createElement('ul');
-    ul.className = 'side-ul';
-    const li = document.createElement('li');
-    li.innerHTML =`  
-      <h2>Products</h2>
-      <h2>About</h2>
-      <h2>Contact</h2>`;
-    ul.appendChild(li);
-    sideMenu.appendChild(ul);
+    sideMenu.style.display = 'block';
     tl.to('.toggle-1' , {rotate: '40', transformOrigin: '20% 40%', duration: 0.5}, 'dissapear')
       .to('.toggle-2', {opacity: 0 , duration: 0.3}, 'dissapear')
       .to('.toggle-3', {rotate: '-40', transformOrigin: '20% 40%', duration: 0.5}, 'dissapear')
       .from('.side-menu', {x: '-100%', duration: 0.4}, 'dissapear')
-      .from('h2', {ease: "back.out(2)", x: '-100%'}, 'dissapear');
+      .from('.side-list li', {ease: "back.out(2)", x: '-100%', stagger: 0.10}, 'dissapear');
     firstToggle = true;
   } else {
     if(!tl.reversed()) {
@@ -32,3 +24,5 @@ toggle.addEventListener('click', (e) => {
     }
   }
 });
+
+// 
